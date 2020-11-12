d7979aa420e63365064a93cf4ab9d5a83a05187d
jim <Jim Evans>
2020-11-12 14:37:42 -0800

    Port over to TTGO-TS board

--- autotrim.ino
+++ autotrim.ino
@@ -29,7 +29,6 @@
 #include "RunningLeastSquares.h"
 #include "PidControl.h"
 #define LED_PIN 2
-#define BUTTON_PIN 0
 
 #ifdef ESP32
 #include "WiFiMulti.h"
@@ -44,18 +43,53 @@ U8G2_SSD1306_128X64_NONAME_F_SW_I2C u8g2(U8G2_R0, /* clock=*/ 15, /* data=*/ 4, 
 
 #endif
 
+#include <queue>
 
-/*
- * 13	Serial2
- * 12	Serial2
- * 14	Relay 1
- * 27	Relay 2
- * 26
- * 25	
- * 33	ADC IN
- * 32	CAN
- * 35	CAN
- */
+struct CanPacket { 
+	uint8_t *buf;
+	int len;
+	int id;
+};
+
+std::queue<struct CanPacket> qFull;
+std::queue<uint8_t *> qEmpty; 
+
+// PINOUT Garmin G5 DB9 connection, G5 manual Appendix A.1.1 p. 156
+// Looking at front of male connector
+// 
+//  1 2 3 4 5		1 CAN-H   	2 CAN-L  	3 UNIT-ID	4 RX	6 TX
+//   6 7 8 9              6 Sig GND 	7. 12V 		8 12V	9 GND
+
+
+// Pinout for custom board - DB9 looking at front of male 
+// 	1 2 3 4 5		1 CAN-H		2 CAN-L		3 ADC IN	4 TX	5 Relay Common
+//   6 7 8 9 				6 Relay1	7. 12V		8 Relay2  9 GND 
+
+// Pinout for TTGO-TS looking at back of board connector left top
+//
+//               VN  VP
+//  ADC (green)	 33	 RST
+//  TX (yellow)  27  32  (yellow)   CAN 
+//               GND 26  (purple)   CAN
+//               00  GND (black)    GND to CAN
+//     (black)   GND 3.3 (red)      Power to CAN
+//               RX  21  (yellow)   relay2
+//               TX  22  (orange)   relay1
+//               VB  5.0 (red) 		Power in
+//
+//
+
+// TTGO Schema 
+const struct { 
+	int ADC = 33;
+	int canTx = 32; /* yellow */
+	int canRx = 26; /* purple */
+	int relay1 = 21;
+	int relay2 = 22;
+	int serialTx = 27; 
+	int serialRx = 0; /* unused */
+	int button = 34;
+} pins;
 
 WiFiUDP udp;
 //const char *udpHost = "192.168.4.100";
@@ -134,8 +168,13 @@ struct {
 	bool forceSend;
 	int lastId, lastSize, exceptions;
 	uint64_t timestamp;
+	uint8_t *bufIn, *bufOut; 
+	int debugDropped;
+	int udpSent;
+	int udpErrs;
 } isrData;
 
+uint8_t canDebugBuf[1024];
 PidControl pid(2);
 static RollingAverage<int,1000> trimPosAvg;
 
@@ -143,8 +182,8 @@ void sendDebugData() {
 	char sbuf[160];				
 	snprintf(sbuf, sizeof(sbuf), "%d %f %s DEBUG\n", 
 		(int)isrData.count, (float)trimPosAvg.average(), WiFi.localIP().toString().c_str());
-	superSend(sbuf);
-	Serial.printf(sbuf);
+	//superSend(sbuf);
+	//Serial.printf(sbuf);
 }
 
 void sendCanData() { 
@@ -154,7 +193,7 @@ void sendCanData() {
 		isrData.pitch, isrData.roll, isrData.magHdg, isrData.magTrack, isrData.ias, isrData.tas, 
 		isrData.palt, isrData.knobSel, isrData.knobVal, age, isrData.mode);
 	superSend(sbuf);
-	Serial.printf("%s", sbuf);
+	//Serial.printf("%d\t%d\t%s", isrData.udpSent, isrData.count, sbuf);
 }
 
 void canPrint(int packetSize) { 
@@ -188,17 +227,29 @@ void canParse(int packetSize) {
 	static int mpSize = 0;
 	static int lastId = 0;
 	// try to parse packet
+	if (packetSize == 0) 
+		Serial.print("0 length packet\n");
 	if (packetSize) {
 		if (CAN.packetId() != lastId || mpSize >= sizeof(ibuf)) {
+			isrData.count++;
+			isrData.lastId = lastId;
+			isrData.lastSize = mpSize;
 			
 			// TODO: move all this complicated debugging output out from the ISR
-			if (udpCanOut) { 
-				udp.beginPacket("255.255.255.255", 7894);
-				udp.write((uint8_t *)&lastId, sizeof(lastId));
-				udp.write((uint8_t *)&mpSize, sizeof(mpSize));
-				udp.write((uint8_t *)ibuf, mpSize);
-				udp.endPacket();
+			if (0 /*udpCanOut*/) { 
+				if (!qEmpty.empty()) {
+					CanPacket pkt;
+					pkt.buf = qEmpty.front();
+					qEmpty.pop();					
+					pkt.id = lastId;
+					pkt.len = mpSize;
+					memcpy(pkt.buf, ibuf, mpSize);
+					qFull.push(pkt);
+				} else { 
+					isrData.debugDropped++;
+				}
 			}		
+
 			if (0) {
 				static char obuf[1024];
 				snprintf(obuf, sizeof(obuf), " (%.3f) can0 %08x [%02d] ", millis() / 1000.0, lastId, mpSize);
@@ -214,15 +265,8 @@ void canParse(int packetSize) {
 				obuf[outlen++] = '\n';
 				obuf[outlen] = 0;				
 				Serial.print(obuf);
-				
-				if (udpCanOut) { 
-					udp.beginPacket("255.255.255.255", 7894);
-					udp.write((uint8_t *)obuf, outlen);
-					udp.endPacket();
-				}		
 			}
-			isrData.lastId = lastId;
-			isrData.lastSize = mpSize;
+			
 			if ((lastId == 0x18882100 || lastId == 0x188c2100) && ibuf[0] == 0xdd && ibuf[1] == 0x00 && mpSize == 60 && ibuf[15] & 0x40) {
 				try {
 					isrData.magHdg = floatFromBinary(&ibuf[16]);
@@ -236,8 +280,7 @@ void canParse(int packetSize) {
 				} catch(...) {
 					isrData.magTrack = 0;
 				}
-			} 
-	
+			} 	
 			if (lastId == 0x18882100 && ibuf[0] == 0xdd && ibuf[1] == 0x00 && mpSize == 60) {
 				// TODO: consider 0x188c? 
 				//Serial.printf("AHRS PACKET\n"); 
@@ -247,7 +290,7 @@ void canParse(int packetSize) {
 					isrData.forceSend = true;
 					isrData.timestamp = millis();
 				} catch(...) {
-					isrData.magHdg = isrData.roll = 0; 
+					isrData.pitch = isrData.roll = 0; 
 					isrData.exceptions++;
 				}
 				//sendCanData(false);
@@ -290,13 +333,14 @@ void canParse(int packetSize) {
 				isrData.forceSend = true;
 				//sendCanData(true);
 			}
+
+			// reset for next packet
 			lastId = CAN.packetId();
 			mpSize = 0;
 		}
 		if (!CAN.packetRtr()) {
 			for (int n = 0; n < packetSize; n++) {
 				ibuf[mpSize + n] = CAN.read();
-				isrData.count++;
 			}
 			mpSize += packetSize;
 		}
@@ -305,7 +349,7 @@ void canParse(int packetSize) {
 
 
 void canInit() {
-	CAN.setPins(35/*rx*/,32/*tx*/);    // 25 and 26 on heltec 
+	CAN.setPins(pins.canRx, pins.canTx);     
 	if (!CAN.begin(1000E3)) {
 		Serial.println("Starting CAN failed!");
 		while (1) {}
@@ -369,21 +413,22 @@ public:
 };
 
 static int cmdCount = 0;
-static PinPulse pp[] = { PinPulse(27), PinPulse(14) };
+static PinPulse pp[] = { PinPulse(pins.relay1), PinPulse(pins.relay2) };
+
 
 void setup() {
 	esp_task_wdt_init(20, true);
-	esp_err_t err = esp_task_wdt_add(NULL);
+	esp_task_wdt_add(NULL);
 
 	Serial.begin(921600, SERIAL_8N1);
 	Serial.setTimeout(10);
 	Serial.printf("AUTOTRIM\n");
 
-	Serial2.begin(9600, SERIAL_8N1, 13/*rx*/, 12/*tx*/);
+	Serial2.begin(9600, SERIAL_8N1, pins.serialRx, pins.serialTx);
 	Serial2.setTimeout(10);
 	
 	pinMode(LED_PIN, OUTPUT);
-	pinMode(BUTTON_PIN, INPUT_PULLUP);
+	pinMode(pins.button, INPUT_PULLUP);
 //	pinMode(3,INPUT);
 #ifdef SCREEN
 	u8g2.begin();
@@ -419,15 +464,23 @@ void setup() {
 
 	udp.begin(udpPortIn);
 	
-	adcAttachPin(33);
+	adcAttachPin(pins.ADC);
 	analogSetCycles(255);
 	pid.setGains(4, 0, 0);
 	pid.finalGain = 1;
+
 	ArduinoOTA.begin();
 	ArduinoOTA.onStart([]() {
+		esp_task_wdt_delete(NULL);
+		CAN.onReceive(NULL);
+		CAN.end();
 		Serial.println("Start");
 		otaInProgress = true;
 	});
+	
+	for (int n = 0; n < 10; n++) {
+		qEmpty.push((uint8_t *)malloc(1024));
+	}
 	canInit();
 }
 
@@ -465,16 +518,31 @@ EggTimer pinReportTimer(200), canResetTimer(5000), sl30Heartbeat(1000);
 uint64_t nextCmdTime = 0;
 static bool debugMoveNeedles = false;
 
-
 static bool first = true;
 void loop() {
 	esp_task_wdt_reset();
 	ArduinoOTA.handle();
 	if (otaInProgress) {
+		esp_task_wdt_init(220, true);
 		CAN.onReceive(NULL);
+		CAN.end();
 		return;
 	}
-	if (first || digitalRead(BUTTON_PIN) == 0) { 
+	while(qFull.empty() == false) {
+		struct CanPacket pkt = qFull.front();
+		qFull.pop();
+		
+		if (udp.beginPacket("255.255.255.255", 7894) == 0) 
+			isrData.udpErrs++;
+		udp.write((uint8_t *)&pkt.id, sizeof(pkt.id));
+		udp.write((uint8_t *)&pkt.len, sizeof(pkt.len));
+		udp.write(pkt.buf, pkt.len);
+		if (udp.endPacket() == 0) 
+			isrData.udpErrs++;
+		isrData.udpSent++;
+		qEmpty.push(pkt.buf);
+	}
+	if (first || digitalRead(pins.button) == 0) { 
 		first = false;
 		digitalWrite(pp[0].pin, 1);
 		delay(200);
@@ -495,7 +563,7 @@ void loop() {
 		CAN.filter(0,0);
 	}
 #ifdef SCREEN
-	if (1 && pp[0].toggleTime == 0 && pp[1].toggleTime == 0 && screenTimer.tick()) { 
+	if (0 && pp[0].toggleTime == 0 && pp[1].toggleTime == 0 && screenTimer.tick()) { 
 		u8g2.setCursor(0,20);
 		u8g2.printf("CMDS: %03d TSET: %03d   ", cmdCount, (int)setPoint);
 		//u8g2.setCursor(0,30);
@@ -509,15 +577,15 @@ void loop() {
 	}
 
 	trimPosAvg.add(analogRead(33));
-	if (pinReportTimer.tick() || isrData.forceSend) { 
+	if (pinReportTimer.tick()/* || isrData.forceSend*/) { 
 		isrData.forceSend = false;
 		//Serial.printf("p%d %d %.2f %.2f m %d\n", lastCmdPin,  (int)(micros() - lastCmdTime), 
 		//	trimPosAvg.average(), startTrimPos, lastCmdMs);
 		sendCanData();
 	}
-	if (0 && serialReportTimer.tick()) {
-		sendDebugData(); 
-		Serial.printf("L: %05.3f/%05.3f/%05.3f\n", loopTimeAvg.average()/1000.0, loopTimeAvg.min()/1000.0, loopTimeAvg.max()/1000.0);
+	if (1 && serialReportTimer.tick()) {
+		//sendDebugData(); 
+		//Serial.printf("L: %05.3f/%05.3f/%05.3f err:%d\n", loopTimeAvg.average()/1000.0, loopTimeAvg.min()/1000.0, loopTimeAvg.max()/1000.0, isrData.udpErrs);
 	}
 
 	if (millis() > nextCmdTime && setPoint != -1) { 
