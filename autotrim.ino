b7fba3bc696c886028b03d38e5a643a8fa14e50e
jim <Jim Evans>
2021-03-24 23:07:27 -0700

    Fix build

--- autotrim.ino
+++ autotrim.ino
@@ -23,7 +23,6 @@ WiFiUDP udpG90;
 GDL90Parser gdl90;
 GDL90Parser::State state;
 
-static const float FEET_PER_METER = 3.3208;
 static float g5KnobValues[6];
 static double hd = -2, vd = -2; // cdi deflections 
 
@@ -503,152 +502,6 @@ void setup() {
 	can.onReceive(canParse);
 	can.begin();
 }
-
-struct LatLon {
-	LatLon(double la, double lo) : lat(la), lon(lo) {}
-	LatLon() {} 
-	double lat;
-	double lon; 
-	LatLon toRadians() { 
-		return LatLon(lat * M_PI/180, lon * M_PI/180); 
-	} 
-	LatLon toDegrees() { 
-		return LatLon(lat * 180/M_PI, lon *180/M_PI); 
-	} 
-};
-
-double bearing(LatLon a, LatLon b) { // returns relative brg in degrees
-	a = a.toRadians();
-	b = b.toRadians();
-	double dL = b.lon - a.lon;
-	double x = cos(b.lat) * sin(dL);
-	double y = cos(a.lat) * sin(b.lat) - sin(a.lat) * 	cos(b.lat)* cos(dL);	
-	double brg = atan2(x, y) * 180/M_PI;
-	if (brg < 0) brg += 360;
-	return brg;
-}
-	
-double distance(LatLon p1, LatLon p2) { // returns distance in meters 
-	p1 = p1.toRadians();
-	p2 = p2.toRadians();
-
-	const double R = 6371e3; // metres
-	double dlat = p2.lat - p1.lat;
-	double dlon = p2.lon - p1.lon;
-	
-	double a = sin(dlat/2) * sin(dlat/2) +
-          cos(p1.lat) * cos(p2.lat) *
-          sin(dlon/2) * sin(dlon/2);
-	double c = 2 * atan2(sqrt(a), sqrt(1-a));
-	return c * R;
-}
-
-LatLon locationBearingDistance(LatLon p, double brng, double d) { 
-	p = p.toRadians();
-	brng *= M_PI/180;
-
-	LatLon p2;
-	const double R = 6371e3; // metres
-	p2.lat = asin(sin(p.lat) * cos(d/R) + cos(p.lat) * sin(d/R) * cos(brng));
-	p2.lon = p.lon + 
-				atan2(sin(brng) * sin(d/R) * cos(p.lat),
-					cos(d/R) - sin(p.lat) * sin(p2.lat));
-	return p2.toDegrees();
-}
-
-double glideslope(float distance, float alt1, float alt2) { 
-		return atan2(alt1 - alt2, distance) * 180/M_PI; 
-}
-
-class IlsSimulator {
-public:
-	const LatLon tdzLocation; // lat/long of touchdown point 
-	const float tdze;  // TDZE elevation of runway 
-	const float faCrs; // final approach course
-	const float gs; // glideslope in degrees
-	float currentAlt;
-	LatLon currentLoc;
-	IlsSimulator(LatLon l, float elev, float crs, float g) : tdzLocation(l), tdze(elev), faCrs(crs), gs(g) {}
-	void setCurrentLocation(LatLon now, double alt) { 
-		currentLoc = now;
-		currentAlt = alt;
-	}
-	double glideSlopeError() {
-		return glideslope(distance(currentLoc, tdzLocation), currentAlt, tdze) - gs;
-	}
-	double courseErr() { 
-		return bearing(currentLoc, tdzLocation) - faCrs;
-	}
-	double cdiPercent() { 
-		return max(-1.0, min(1.0, courseErr() / 2.5));
-	}
-	double gsPercent() { 
-		return max(-1.0, min(1.0, glideSlopeError() / 0.7));
-	}
-} *ils = NULL;
-
-struct  Approach{
-	const char *name;
-	double lat, lon, fac, tdze, gs;
-};
-
-Approach approaches[] = {
-	{"KBFI 14R", 47.53789999126084, -122.30912681366567, 135.0, 18, 3.00}, 
-	{"KBFI 32L", 47.52145193515430, -122.29521847198963, 315.0, 18, 3.00}, 
-	{"S43 15",   47.90660383843286, -122.10299154204903, 150.0, 22, 4.00},
-	{"S43 33",   47.90250761678649, -122.10136258707182, 328.0, 22, 4.00},
-	{"2S1 17",   47.46106431485166, -122.47652028295599, 170.0, 300, 4.00}, 
-	{"2S1 35",   47.45619317486062, -122.47745151953475, 350.0, 300, 4.00}, 
-	{"WN14 02",  47.46276277164776, -122.56987914788057, 040.0, 300, 4.50},	
-};
-	
-	
-float angularDiff(float a, float b) { 
-	float d = a - b;
-	if (d > +180) d -= 360;
-	if (d < -180) d += 360;
-	return d;
-}
-
-float magToTrue(float ang) { 
-	return ang + 15.5;		;
-}
-
-float trueToMag(float ang) { 
-	return ang - 15.5;		;
-}
-
-struct LatLonAlt { 
-	LatLon loc;
-	float alt; // meters 
-	bool valid;
-	LatLonAlt() : valid(false) {}
-	LatLonAlt(LatLon l, float a) : loc(l), alt(a), valid(true) {}
-	LatLonAlt(double lat, double lon, float a) : loc(lat, lon), alt(a), valid(true) {};
-};
-
-Approach *findBestApproach(LatLon p) { 
-	Approach *best = NULL;
-	for(int n = 0; n < sizeof(approaches)/sizeof(approaches[0]); n++) {
-		Approach &a = approaches[n];
-		LatLon tdz = LatLon(a.lat, a.lon); 
-		float brg = bearing(p, LatLon(a.lat, a.lon));
-		float dist = distance(p, tdz);
-		if (abs(angularDiff(brg, a.fac)) >= 60 || dist > 20000)
-			continue;
-		if (best == NULL || 
-			distance(p, tdz) < distance(p, LatLon(best->lat, best->lon)))
-			best = &approaches[n]; 
-	}
-	if (best != NULL) { 
-		Serial.printf("Chose '%s', bearing %.1f, dist %.1f\n", best->name, 
-			bearing(p, LatLon(best->lat, best->lon)), distance(p, LatLon(best->lat, best->lon)));
-	} else { 
-		Serial.printf("No best approach?\n");
-	}
-	return best;
-}
-
 GDL90Parser::State currentState;
 
 static EggTimer report(2000);
@@ -923,7 +776,6 @@ void loop() {
 ////////////////////////////////////////////////////////////////////////////////////////
 // the rest of the file is simulation support code 
 
-
 class TrackSimulator {
   public: 
 	LatLonAlt curPos;
@@ -932,7 +784,7 @@ class TrackSimulator {
 	bool waypointPassed;
 	float speed; // knots 
 	float vvel;  // fpm
-	float track;
+	float steerHdg, track;
 	float lastHd, lastVd;
 	float corrH = 0, corrV = 0;
 	float hWiggle = 0, vWiggle = 0; // add simulated hdg/alt variability
@@ -944,27 +796,30 @@ class TrackSimulator {
 		lastHd = hd;
 		lastVd = vd;
 	}
+
 	void run(float sec) { 
 		float distToWaypoint = 0;
 		if (!curPos.valid)
 			return;
 		if (activeWaypoint.valid && !waypointPassed) {
-			track = bearing(curPos.loc, activeWaypoint.loc) + hWiggle;
+			steerHdg = bearing(curPos.loc, activeWaypoint.loc) + hWiggle;
 			distToWaypoint = distance(curPos.loc, activeWaypoint.loc);
 		}
-		float dist = speed * .51444 * sec;
-		LatLon newPos = locationBearingDistance(curPos.loc, track, dist);
-
+		float distTravelled = speed * .51444 * sec;
 		float newAlt = curPos.alt;
+
 		if (distToWaypoint > 0 ) 
-			newAlt += (activeWaypoint.alt - curPos.alt) * (dist / distToWaypoint) + vWiggle;
-		track += corrH;//distToWaypoint / 1000;
+			newAlt += (activeWaypoint.alt - curPos.alt) * (distTravelled / distToWaypoint) + vWiggle;
+		steerHdg += corrH;//distToWaypoint / 1000;
 		newAlt += corrV;//distToWaypoint / 1000;
 		vvel = (newAlt - curPos.alt) / sec * 196.85; // m/s to fpm 
+		track = onNavigate(steerHdg);
+		LatLon newPos = locationBearingDistance(curPos.loc, track, distTravelled);
 		curPos = LatLonAlt(newPos, newAlt);
-		if (abs(angularDiff(track, bearing(curPos.loc, activeWaypoint.loc))) >= 90)
+		if (abs(angularDiff(steerHdg, bearing(curPos.loc, activeWaypoint.loc))) >= 90)
 			waypointPassed = true;
 	}	
+	function<float(float)> onNavigate = [](float steer){ return steer; };
 	void setWaypoint(const LatLonAlt &p) {
 		activeWaypoint = p;
 		waypointPassed = false;
@@ -1012,7 +867,7 @@ public:
 				g5KnobValues[knobSel] = knobVal; 
 			if (sscanf(s.c_str(), "%lf, %lf %lf %lf", &lat, &lon, &alt, &track) == 4) {  
 				sim.setWaypoint(LatLonAlt(lat, lon, alt / FEET_PER_METER));
-				sim.track = track;
+				sim.steerHdg = track;
 			} else if (sscanf(s.c_str(), "%lf, %lf %lf", &lat, &lon, &alt) == 3) 
 				sim.setWaypoint(LatLonAlt(lat, lon, alt / FEET_PER_METER));
 			else if (sscanf(s.c_str(), "%lf, %lf", &lat, &lon) == 2) 
@@ -1074,7 +929,7 @@ class ESP32sim_autotrim : ESP32sim_Module {
 				s.lat = p.loc.lat;
 				s.lon = p.loc.lon;
 				s.alt = p.alt;
-				s.track = tSim.sim.track;
+				s.track = tSim.sim.steerHdg;
 				s.vvel = tSim.sim.vvel;
 				s.hvel = tSim.sim.speed;
 				s.palt = (p.alt + 1000) / 25;
