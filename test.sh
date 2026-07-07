548597f480855dd82675dacf98a95bdff13a4815
Jim Evans <jim@vheavy.com>
2026-07-07 12:52:22 -0700

    Change starting location in test.sh to fix bug where simulation accidentally picks up the wrong simulated approach out of the approach list.  I wanted it to pick KBFI.   Change the test duration to give the simulation time to barely arrive at the runway.   Test behavior is not what is expected, it appears that the simulated flight just follows the waypoints listed in test.sh, not follow the CDI needles for the simulated ILS approach.

--- test.sh
+++ test.sh
@@ -7,12 +7,12 @@ AP 1
 INPUT.ALTBUG 0
 INPUT.VLOCBUG 0
 INPUT.MODE 5
-47.59509212379994, -122.38743386638778 1000
+47.679140933777035, -122.33932295359429 1000
 47.53789999126084, -122.30912681366567 50
 
 EOF
 
 make BOARD=csim -j2 && \
-./autotrim_csim --seconds 163.5 --tracksim ./tracksim.txt > out.txt && \
+./autotrim_csim --seconds 320 --tracksim ./tracksim.txt > out.txt && \
 awk '/TSIM/ { line=$0; mode=$5; range=$19 } END { print line; exit !(mode == 5 && range > 0 && range < 350) }' out.txt
  
